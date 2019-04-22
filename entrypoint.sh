8873e5fb407a5be94403b5f4bae41b1a2c51b706
TheMiloAnderson <themiloanderson@gmail.com>
2019-04-22 09:24:10 -0700

    docker setup

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+set -e
+
+cd /src
+
+python3 manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py collectstatic
+python3 manage.py runserver 0.0.0.0:8000
