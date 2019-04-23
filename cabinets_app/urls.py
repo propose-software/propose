87723a7206375980b68f75f432bcda0e5969e75e
TheMiloAnderson <themiloanderson@gmail.com>
2019-04-22 17:30:15 -0700

    project_create form/view/template

--- cabinets_app/urls.py
+++ cabinets_app/urls.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .views import (
-    account, project_list, project_detail, cabinet_detail_view, spec_detail_view
+    account, project_list, project_detail,
+    project_create, cabinet_detail_view, spec_detail_view
 )
 from django.urls import path
 
 urlpatterns = [
     path('', account, name='account'),
     path('account/<int:account_id>', project_list, name='project_list'),
+    path('project/', project_create, name='project_create'),
     path('project/<int:proj_id>', project_detail, name='project_detail'),
     path('2/cabinet/2', cabinet_detail_view, name='cabinet_detail_view'),
     path('3/specification/3', spec_detail_view, name='spec_detail_view')
--- cabinets_app/views.py
+++ cabinets_app/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import (
     Account, Material, Labor,
     Specification, Cabinet, Drawer,
     Hardware, Project
+)
+from .forms import (
+    ProjectForm
 )
 
 
@@ -21,6 +24,20 @@ def project_list(req, account_id=None):
         'projects': Project.objects.filter(account__id=account_id)
     }
     return render(req, './project/project_list.html', context)
+
+
+@login_required
+def project_create(req):
+    if req.method == 'POST':
+        form = ProjectForm(req.POST)
+        if form.is_valid():
+            new_project = form.save()
+            return redirect('project/' + str(new_project.id))
+        else:
+            return render(req, './project/project_create.html', {'form': form})
+    else:
+        form = ProjectForm()
+        return render(req, './project/project_create.html', {'form': form})
 
 
 @login_required
