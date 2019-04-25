f51be92d60d84469c29c1905afeac2ae8de0c740
Paul Williamsen <pwilliamsen@me.com>
2019-04-25 10:26:54 -0700

    material testing

--- cabinets_app/tests_views.py
+++ cabinets_app/tests_views.py
@@ -59,3 +59,73 @@ class TestAccounts(TestCase):
         account_info = get_account_info()
         res = self.client.post('/account/', account_info, follow=True)
         self.assertIn(account_info['name'].encode(), res.content)
+
+class TestMaterials(TestCase):
+
+    def setUp(self):
+        self.user = UserFactory()
+        self.user.set_password('12345')
+        self.user.save()
+        self.client = Client()
+        self.client.login(
+            username=self.user.username,
+            password='12345'
+        )
+
+    def test_material_create_get(self):
+        res = self.client.get('/material', follow=True)
+        self.assertIn(b'<h2>Create Material</h2>', res.content)
+
+
+    def test_material_create_post(self):
+        material_create = get_material_info()
+        res = self.client.post('/material', material_create, follow=True)
+        self.assertIn('<h2>Create Material</h2>', res.content.decode())
+
+    def test_material_list(self):
+        material = Material.objects.create(**get_material_info())
+        res = self.client.get('/material/' + str(material.id), follow=True)
+        self.assertIn(material.name.encode(), res.content)
+
+    def test_material_detail(self):
+        material = Material.objects.create(**get_material_info())
+        res = self.client.get('/material/' + str(material.id), follow=True)
+        self.assertIn(material.name.encode(), res.content)
+        self.assertIn(material.description.encode(), res.content)
+        self.assertIn(str(material.thickness).encode(), res.content)
+        self.assertIn(str(material.width).encode(), res.content)
+        self.assertIn(str(material.length).encode(), res.content)
+        self.assertIn(str(material.sheet_cost).encode(), res.content)
+        self.assertIn(str(material.waste_factor).encode(), res.content)
+        self.assertIn(str(material.markup).encode(), res.content)
+
+class TestHardware(TestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        self.user.set_password('12345')
+        self.user.save()
+        self.client = Client()
+        self.client.login(
+            username=self.user.username,
+            password='12345'
+        )
+class TestProjects(TestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        self.user.set_password('12345')
+        self.user.save()
+        self.client = Client()
+        self.client.login(
+            username=self.user.username,
+            password='12345'
+        )
+class TestCabinets(TestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        self.user.set_password('12345')
+        self.user.save()
+        self.client = Client()
+        self.client.login(
+            username=self.user.username,
+            password='12345'
+        )
