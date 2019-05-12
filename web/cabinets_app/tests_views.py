40766bda972524ceb1652894d277735b09861d10
TheMiloAnderson <themiloanderson@gmail.com>
2019-05-12 10:53:15 -0700

    Added Labor views tests; fixed Account, Material, Hardware tests
    Updated views tests for create-post and update-post so they do a better job of checking
    form validation, and checking that object fields were really updated

--- web/cabinets_app/tests_views.py
+++ web/cabinets_app/tests_views.py
@@ -58,9 +58,16 @@ class TestMaterials(TestCase):
         self.assertIn(b'<h2>Create Material</h2>', res.content)
 
     def test_material_create_post(self):
-        material_create = get_material_info()
-        res = self.client.post('/material', material_create, follow=True)
-        self.assertIn('<h2>Create Material</h2>', res.content.decode())
+        material_info = get_material_info()
+        res = self.client.post('/material/', material_info, follow=True)
+        self.assertIn(material_info['name'], res.content.decode())
+        self.assertIn(material_info['description'], res.content.decode())
+        self.assertIn(str(material_info['thickness']), res.content.decode())
+        self.assertIn(str(material_info['width']), res.content.decode())
+        self.assertIn(str(material_info['length']), res.content.decode())
+        self.assertIn(str(material_info['sheet_cost']), res.content.decode())
+        self.assertIn(str(material_info['waste_factor']), res.content.decode())
+        self.assertIn(str(material_info['markup']), res.content.decode())
 
     def test_material_update_get(self):
         material = Material.objects.create(**get_material_info())
@@ -72,9 +79,20 @@ class TestMaterials(TestCase):
         material_info = get_material_info()
         material = Material.objects.create(**material_info)
         material_info['name'] = 'New Material Name'
+        material_info['description'] = 'New Description'
+        material_info['thickness'] = 99
+        material_info['width'] = 100
+        material_info['length'] = 101
         url = '/material/' + str(material.id) + '/update'
         res = self.client.post(url, material_info, follow=True)
-        self.assertIn(material_info['name'].encode(), res.content)
+        self.assertIn(material_info['name'], res.content.decode())
+        self.assertIn(material_info['description'], res.content.decode())
+        self.assertIn(str(material_info['thickness']), res.content.decode())
+        self.assertIn(str(material_info['width']), res.content.decode())
+        self.assertIn(str(material_info['length']), res.content.decode())
+        self.assertIn(str(material_info['sheet_cost']), res.content.decode())
+        self.assertIn(str(material_info['waste_factor']), res.content.decode())
+        self.assertIn(str(material_info['markup']), res.content.decode())
 
     def test_material_delete_get(self):
         material = Material.objects.create(**get_material_info())
@@ -92,6 +110,7 @@ class TestMaterials(TestCase):
 
 
 class TestHardware(TestCase):
+
     def setUp(self):
         self.user = UserFactory()
         self.user.set_password('12345')
@@ -112,7 +131,7 @@ class TestHardware(TestCase):
         res = self.client.get('/hardware/' + str(hardware.id), follow=True)
         self.assertIn(hardware.name.encode(), res.content)
         self.assertIn(str(hardware.cost_per).encode(), res.content)
-        self.assertIn(str(hardware.unit_type).encode(), res.content)
+        self.assertIn(hardware.unit_type.encode(), res.content)
         self.assertIn(str(hardware.markup).encode(), res.content)
 
     def test_hardware_create_get(self):
@@ -120,9 +139,12 @@ class TestHardware(TestCase):
         self.assertIn(b'<title>Create Hardware</title>', res.content)
 
     def test_hardware_create_post(self):
-        hardware_create = get_hardware_info()
-        res = self.client.post('/hardware', hardware_create, follow=True)
-        self.assertIn('<h2>Create Hardware</h2>', res.content.decode())
+        hardware_info = get_hardware_info()
+        res = self.client.post('/hardware/', hardware_info, follow=True)
+        self.assertIn(hardware_info['name'], res.content.decode())
+        self.assertIn(str(hardware_info['cost_per']), res.content.decode())
+        self.assertIn(hardware_info['unit_type'], res.content.decode())
+        self.assertIn(str(hardware_info['markup']), res.content.decode())
 
     def test_hardware_update_get(self):
         hardware = Hardware.objects.create(**get_hardware_info())
@@ -134,9 +156,15 @@ class TestHardware(TestCase):
         hardware_info = get_hardware_info()
         hardware = Hardware.objects.create(**hardware_info)
         hardware_info['name'] = 'New Test Name'
+        hardware_info['cost_per'] = 3.14
+        hardware_info['unit_type'] = 'Set'
+        hardware_info['markup'] = .33
         url = '/hardware/' + str(hardware.id) + '/update'
         res = self.client.post(url, hardware_info, follow=True)
-        self.assertIn(hardware_info['name'].encode(), res.content)
+        self.assertIn(hardware_info['name'], res.content.decode())
+        self.assertIn(str(hardware_info['cost_per']), res.content.decode())
+        self.assertIn(hardware_info['unit_type'], res.content.decode())
+        self.assertIn(str(hardware_info['markup']), res.content.decode())
 
     def test_hardware_delete_get(self):
         hardware = Hardware.objects.create(**get_hardware_info())
@@ -151,6 +179,71 @@ class TestHardware(TestCase):
         self.client.post('/hardware/' + str(hardware.id) + '/delete')
         res = self.client.get('/', follow=True)
         self.assertNotIn(hardware.name.encode(), res.content)
+
+
+class TestLabor(TestCase):
+
+    def setUp(self):
+        self.user = UserFactory()
+        self.user.set_password('12345')
+        self.user.save()
+        self.client = Client()
+        self.client.login(
+            username=self.user.username,
+            password='12345'
+        )
+
+    def test_labor_list(self):
+        labor = Labor.objects.create(**get_labor_info())
+        res = self.client.get('/labor/' + str(labor.id), follow=True)
+        self.assertIn(labor.item_name.encode(), res.content)
+
+    def test_labor_detail(self):
+        labor = Labor.objects.create(**get_labor_info())
+        res = self.client.get('/labor/' + str(labor.id), follow=True)
+        self.assertIn(labor.item_name.encode(), res.content)
+        self.assertIn(str(labor.minutes).encode(), res.content)
+        self.assertIn(labor.unit_type.encode(), res.content)
+
+    def test_labor_create_get(self):
+        res = self.client.get('/labor', follow=True)
+        self.assertIn(b'<title>Create Labor</title>', res.content)
+
+    def test_labor_create_post(self):
+        labor_info = get_labor_info()
+        res = self.client.post('/labor/', labor_info, follow=True)
+        self.assertIn(labor_info['item_name'], res.content.decode())
+        self.assertIn(str(labor_info['minutes']), res.content.decode())
+
+    def test_labor_update_get(self):
+        labor = Labor.objects.create(**get_labor_info())
+        url = '/labor/' + str(labor.id) + '/update'
+        res = self.client.get(url, follow=True)
+        self.assertIn(labor.item_name.encode(), res.content)
+
+    def test_labor_update_post(self):
+        labor_info = get_labor_info()
+        labor = Labor.objects.create(**labor_info)
+        labor_info['item_name'] = 'New Test Name'
+        labor_info['minutes'] = 123
+        url = '/labor/' + str(labor.id) + '/update'
+        res = self.client.post(url, labor_info, follow=True)
+        self.assertIn(labor_info['item_name'], res.content.decode())
+        self.assertIn(str(labor_info['minutes']), res.content.decode())
+
+    def test_labor_delete_get(self):
+        labor = Labor.objects.create(**get_labor_info())
+        url = '/labor/' + str(labor.id) + '/delete'
+        res = self.client.get(url, follow=True)
+        self.assertIn(labor.item_name.encode(), res.content)
+
+    def test_labor_delete_post(self):
+        labor = Labor.objects.create(**get_labor_info())
+        res = self.client.get('/labor/all', follow=True)
+        self.assertIn(labor.item_name.encode(), res.content)
+        self.client.post('/labor/' + str(labor.id) + '/delete')
+        res = self.client.get('/', follow=True)
+        self.assertNotIn(labor.item_name.encode(), res.content)
 
 
 class TestAccounts(TestCase):
@@ -188,6 +281,11 @@ class TestAccounts(TestCase):
         account_info = get_account_info()
         res = self.client.post('/account/', account_info, follow=True)
         self.assertIn(account_info['name'].encode(), res.content)
+        self.assertIn(account_info['billing_address'].encode(), res.content)
+        self.assertIn(account_info['billing_phone'].encode(), res.content)
+        self.assertIn(account_info['billing_email'].encode(), res.content)
+        self.assertIn(account_info['contact_name'].encode(), res.content)
+        self.assertIn(str(account_info['discount']).encode(), res.content)
 
     def test_account_update_get(self):
         account = Account.objects.create(**get_account_info())
@@ -199,9 +297,16 @@ class TestAccounts(TestCase):
         account_info = get_account_info()
         account = Account.objects.create(**account_info)
         account_info['name'] = 'New Test Name'
+        account_info['billing_address'] = '1600 Pennsylvania Ave.'
+        account_info['discount'] = 0
         url = '/account/' + str(account.id) + '/update'
         res = self.client.post(url, account_info, follow=True)
         self.assertIn(account_info['name'].encode(), res.content)
+        self.assertIn(account_info['billing_address'].encode(), res.content)
+        self.assertIn(account_info['billing_phone'].encode(), res.content)
+        self.assertIn(account_info['billing_email'].encode(), res.content)
+        self.assertIn(account_info['contact_name'].encode(), res.content)
+        self.assertIn(str(account_info['discount']).encode(), res.content)
 
     def test_account_delete_get(self):
         account = Account.objects.create(**get_account_info())
