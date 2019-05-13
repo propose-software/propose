5032be606b12bccf962d2eba3fa6adfcaa38b00a
TheMiloAnderson <themiloanderson@gmail.com>
2019-05-13 09:59:15 -0700

    Added Room views tests; eliminated pesky datetime warning

--- web/cabinets_app/tests/tests_views_room.py
+++ web/cabinets_app/tests/tests_views_room.py
@@ -1,13 +1,6 @@
 from django.test import TestCase, Client
-from ..models import (
-    Material, Hardware, Labor, Account,
-    Project, Cabinet, Drawer, Specification, Room
-)
-from .tests_data import (
-    get_material_info, get_hardware_info, get_labor_info,
-    get_account_info, get_project_info, get_spec_info, get_room_info,
-    get_cabinet_info, get_drawer_info, UserFactory
-)
+from ..models import Project, Room
+from .tests_data import get_project_info, get_room_info, UserFactory
 
 
 class TestRooms(TestCase):
@@ -21,3 +14,53 @@ class TestRooms(TestCase):
             username=self.user.username,
             password='12345'
         )
+
+    def test_room_detail(self):
+        room = Room.objects.create(**get_room_info())
+        url = '/project/' + str(room.project.id) + '/detail'
+        res = self.client.get(url, follow=True)
+        self.assertIn(room.name.encode(), res.content)
+
+    def test_room_create_get(self):
+        project = Project.objects.create(**get_project_info())
+        url = '/project/' + str(project.id) + '/room'
+        res = self.client.get(url, follow=True)
+        self.assertIn(b'<title>Create Room</title>', res.content)
+
+    def test_room_create_post(self):
+        room_info = get_room_info()
+        url = '/project/' + str(room_info['project'].id) + '/room/'
+        res = self.client.post(url, room_info, follow=True)
+        self.assertIn(room_info['name'].encode(), res.content)
+        self.assertIn(room_info['project'].name.encode(), res.content)
+        self.assertTrue(Room.objects.get(name=room_info['name']))
+
+    def test_room_update_get(self):
+        room = Room.objects.create(**get_room_info())
+        url = '/project/' + str(room.project.id) + '/room/' + str(room.id) + '/update'
+        res = self.client.get(url, follow=True)
+        self.assertIn(room.name.encode(), res.content)
+
+    def test_room_update_post(self):
+        room_info = get_room_info()
+        room = Room.objects.create(**room_info)
+        room_info['name'] = 'New Name'
+        url = '/project/' + str(room.project.id) + '/room/' + str(room.id) + '/update'
+        res = self.client.post(url, room_info, follow=True)
+        self.assertIn(room_info['name'].encode(), res.content)
+
+    def test_room_delete_get(self):
+        room = Room.objects.create(**get_room_info())
+        url = '/project/' + str(room.project.id) + '/room/' + str(room.id) + '/delete'
+        res = self.client.get(url, follow=True)
+        self.assertIn(room.name.encode(), res.content)
+
+    def test_room_delete_post(self):
+        room = Room.objects.create(**get_room_info())
+        project_detail = '/project/' + str(room.project.id) + '/detail'
+        res = self.client.get(project_detail, follow=True)
+        self.assertIn(room.name.encode(), res.content)
+        url = '/project/' + str(room.project.id) + '/room/' + str(room.id) + '/delete'
+        self.client.post(url, follow=True)
+        res = self.client.get(project_detail, follow=True)
+        self.assertNotIn(room.name.encode(), res.content)
