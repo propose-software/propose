539270b26d3509a99a424646ecb59c77ab738b46
Chris Ball <chrisball7@gmail.com>
2019-05-17 15:29:55 -0700

    got the custom user model working locally

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 cd /src
 
 # python3 manage.py migrate
-# python3 manage.py makemigrations
-# python3 manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
 python3 manage.py collectstatic --noinput
 python3 manage.py runserver 0.0.0.0:8000
